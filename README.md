11f9d9d795eeb2a0779d8be1277c7f92a03a7a87
Peter Kieltyka <peter.kieltyka@gmail.com>
2025-03-12 12:05:54 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# node-fetch-token-balances
+node-fetch-token-balances
+=========================
+
 Example of using Sequence Indexer from node+typescript to fetch wallet balances
+
+## Usage
+
+1. `pnpm install`
+2. `pnpm dev`
+
+
+## Tips
+
+Finally, if you'd like to use this for your project, get your own projectAccessKey
+from https://sequence.build and update index.ts.
---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import { SequenceIndexerGateway, Page } from "@0xsequence/indexer";
+
+// get yours at https://sequence.build
+const projectAccessKey = 'AQAAAAAAAF_JvPALhBthL7VGn6jV0YDqaFY'
+
+const main = async () => {
+  const indexer = new SequenceIndexerGateway("https://indexer.sequence.app", projectAccessKey)
+
+
+  let page: Page = {pageSize: 5}
+
+  while (true) {
+    const { balances, page: nextPage } = await indexer.getTokenBalances({
+      accountAddress: "0x8e3E38fe7367dd3b52D1e281E4e8400447C8d8B9",
+      page
+    })
+
+    // print balances
+    console.log('=> balances found:', balances.length)
+    // console.log('=> balances:', balances)
+
+    // query for next page or finish
+    if (!nextPage.more) {
+      // no more pages, we're done.
+      break
+    } else {
+      // set cursor to next page and query again
+      page = nextPage
+    }
+  }
+
+}
+
+main()
+
